0e5f00ec7c21a7de4ce1927c98380b0a248a611a
Nicolas JEUDY <njeudy@tuxservices.com>
2013-03-08 13:30:15 +0100

    new: initial commit of bootstrap intégration in OpenERP

---
+++ __openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name" : 'Add Bootstrap from Twitter',
+    "version" : "0.1",
+    "depends" : ['base',],
+    "author" : "Nicolas JEUDY <njeudy@txs.fr>",
+    "installable" : True,
+    "active" : False,
+    "data": [
+    ],
+    "css": [
+        'static/src/bootstrap/css/bootstrap.min.css',
+        'static/src/bootstrap/css/bootstrap-responsive.min.css',
+        'static/src/css/base.css',
+    ],
+    "js": [
+        'static/src/bootstrap/js/bootstrap.min.js'
+    ],
+    "sequence": 90,
+}
+
