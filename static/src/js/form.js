85b652afec5498d7435427443855bf31ba693160
Nicolas JEUDY <njeudy@tuxservices.com>
2013-05-14 16:05:02 +0200

    new: replace menu with bootstrap navbar fixed top

--- static/src/js/form.js
+++ static/src/js/form.js
@@ -20,5 +20,36 @@ openerp.web_bootstrap = function (oe) {
         }
 
     });
+
+    oe.web.Menu = oe.web.Menu.extend({
+
+      start: function() {
+          this._super.apply(this, arguments);
+          this.$secondary_menus = this.getParent().$el.find('.oe_secondary_menus_container');
+          //NJEUDY: remove onclick action
+          //this.$secondary_menus.on('click', 'a[data-menu]', this.on_menu_click);
+          return this.do_reload();
+      },
+
+      menu_loaded: function(data) {
+          var self = this;
+          this.data = {data: data};
+          this.renderElement();
+          // NJEUDY: auto action on menu for new menu
+          //this.$secondary_menus.html(QWeb.render("Menu.secondary", { widget : this }));
+          //this.$el.on('click', 'a[data-menu]', this.on_menu_click);
+
+          // Hide second level submenus
+          this.$secondary_menus.find('.oe_menu_toggler').siblings('.oe_secondary_submenu').hide();
+          if (self.current_menu) {
+              self.open_menu(self.current_menu);
+          }
+          this.trigger('menu_loaded', data);
+          this.has_been_loaded.resolve();
+      }, 
+
+
+    });
+
 };
 
